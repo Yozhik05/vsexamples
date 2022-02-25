6b5e1c8be7c315cef4f6163315ff08db443876f4
Dmitry <minecraft@yandex.ru>
2022-02-25 22:28:04 +0300

    Добавили калькулятор

---
+++ Calclib/Calc.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Calclib
+{
+    public static class Calc
+    {
+        public static int Sum(int a, int b) => a + b;
+    }
+}
---
+++ TestConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int x = 10;
+            int y = x+20;
+
+            Console.WriteLine("Hi");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
